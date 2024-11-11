f4546e483c93e2e959fdc3f74fa205eb6341ced3
Maruqes <m4rques1s0n@gmail.com>
2024-11-11 21:12:30 +0000

    need to study fast commit

--- Tokenize.go
+++ Tokenize.go
@@ -1,6 +1,12 @@
 package Tokenize
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+)
 
 //tipos de admin -> 0:superadmin, 1:admin, -1:sem acesso
 //para registrar tens de ser admin 0
@@ -17,6 +23,19 @@ import "fmt"
 //uma subricicao
 //dar duracao a subscricao
 
+func PrintEnv() {
+	envVars := []string{"PUBLISHABLE_KEY", "SECRET_KEY", "SUBSCRIPTION_PRICE_ID"}
+	for _, envVar := range envVars {
+		value := os.Getenv(envVar)
+		if value == "" {
+			log.Panicf("%s is not set", envVar)
+		} else {
+			fmt.Printf("%s: %s\n\n", envVar, value)
+		}
+	}
+}
+
 func Init() {
 	fmt.Println("Init")
+	PrintEnv()
 }
