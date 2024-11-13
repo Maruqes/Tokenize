cce7c20211407ff69a89a89e23689ad0c4698241
Maruqes <m4rques1s0n@gmail.com>
2024-11-13 00:32:58 +0000

    test code from stripe, need to read this and test A LOT

--- Tokenize.go
+++ Tokenize.go
@@ -1,11 +1,18 @@
 package Tokenize
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/stripe/stripe-go/v81"
+	portalsession "github.com/stripe/stripe-go/v81/billingportal/session"
+	"github.com/stripe/stripe-go/v81/checkout/session"
+	"github.com/stripe/stripe-go/v81/webhook"
 )
 
 //tipos de admin -> 0:superadmin, 1:admin, -1:sem acesso
@@ -23,19 +30,166 @@ import (
 //uma subricicao
 //dar duracao a subscricao
 
-func PrintEnv() {
-	envVars := []string{"PUBLISHABLE_KEY", "SECRET_KEY", "SUBSCRIPTION_PRICE_ID"}
-	for _, envVar := range envVars {
-		value := os.Getenv(envVar)
-		if value == "" {
-			log.Panicf("%s is not set", envVar)
-		} else {
-			fmt.Printf("%s: %s\n\n", envVar, value)
-		}
+func createCheckoutSession(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
 	}
+
+	r.ParseForm()
+
+	domain := "http://localhost:4242"
+
+	price := &stripe.Price{
+		ID: os.Getenv("SUBSCRIPTION_PRICE_ID"),
+	}
+
+	checkoutParams := &stripe.CheckoutSessionParams{
+		Mode: stripe.String(string(stripe.CheckoutSessionModeSubscription)),
+		LineItems: []*stripe.CheckoutSessionLineItemParams{
+			{
+				Price:    stripe.String(price.ID),
+				Quantity: stripe.Int64(1),
+			},
+		},
+		SuccessURL: stripe.String(domain + "/success.html?session_id={CHECKOUT_SESSION_ID}"),
+		CancelURL:  stripe.String(domain + "/cancel.html"),
+	}
+
+	s, err := session.New(checkoutParams)
+	if err != nil {
+		log.Printf("session.New: %v", err)
+	}
+
+	http.Redirect(w, r, s.URL, http.StatusSeeOther)
+}
+
+func createPortalSession(w http.ResponseWriter, r *http.Request) {
+	domain := "http://localhost:4242"
+	// For demonstration purposes, we're using the Checkout session to retrieve the customer ID.
+	// Typically this is stored alongside the authenticated user in your database.
+	r.ParseForm()
+	sessionId := r.PostFormValue("session_id")
+
+	fmt.Print(sessionId)
+	s, err := session.Get(sessionId, nil)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("session.Get: %v", err)
+		return
+	}
+
+	// Authenticate your user.
+	params := &stripe.BillingPortalSessionParams{
+		Customer:  stripe.String(s.Customer.ID),
+		ReturnURL: stripe.String(domain),
+	}
+	ps, _ := portalsession.New(params)
+	log.Printf("ps.New: %v", ps.URL)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("ps.New: %v", err)
+		return
+	}
+
+	http.Redirect(w, r, ps.URL, http.StatusSeeOther)
+}
+
+func handleWebhook(w http.ResponseWriter, req *http.Request) {
+	const MaxBodyBytes = int64(65536)
+	bodyReader := http.MaxBytesReader(w, req.Body, MaxBodyBytes)
+	payload, err := ioutil.ReadAll(bodyReader)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading request body: %v\n", err)
+		w.WriteHeader(http.StatusServiceUnavailable)
+		return
+	}
+	// Replace this endpoint secret with your endpoint's unique secret
+	// If you are testing with the CLI, find the secret by running 'stripe listen'
+	// If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+	// at https://dashboard.stripe.com/webhooks
+	endpointSecret := "whsec_12345"
+	signatureHeader := req.Header.Get("Stripe-Signature")
+	event, err := webhook.ConstructEvent(payload, signatureHeader, endpointSecret)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "⚠️  Webhook signature verification failed. %v\n", err)
+		w.WriteHeader(http.StatusBadRequest) // Return a 400 error on a bad signature
+		return
+	}
+	// Unmarshal the event data into an appropriate struct depending on its Type
+	switch event.Type {
+	case "customer.subscription.deleted":
+		var subscription stripe.Subscription
+		err := json.Unmarshal(event.Data.Raw, &subscription)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing webhook JSON: %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		log.Printf("Subscription deleted for %s.", subscription.ID)
+		// Then define and call a func to handle the deleted subscription.
+		// handleSubscriptionCanceled(subscription)
+	case "customer.subscription.updated":
+		var subscription stripe.Subscription
+		err := json.Unmarshal(event.Data.Raw, &subscription)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing webhook JSON: %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		log.Printf("Subscription updated for %s.", subscription.ID)
+		// Then define and call a func to handle the successful attachment of a PaymentMethod.
+		// handleSubscriptionUpdated(subscription)
+	case "customer.subscription.created":
+		var subscription stripe.Subscription
+		err := json.Unmarshal(event.Data.Raw, &subscription)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing webhook JSON: %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		log.Printf("Subscription created for %s.", subscription.ID)
+		// Then define and call a func to handle the successful attachment of a PaymentMethod.
+		// handleSubscriptionCreated(subscription)
+	case "customer.subscription.trial_will_end":
+		var subscription stripe.Subscription
+		err := json.Unmarshal(event.Data.Raw, &subscription)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing webhook JSON: %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		log.Printf("Subscription trial will end for %s.", subscription.ID)
+		// Then define and call a func to handle the successful attachment of a PaymentMethod.
+		// handleSubscriptionTrialWillEnd(subscription)
+	case "entitlements.active_entitlement_summary.updated":
+		var subscription stripe.Subscription
+		err := json.Unmarshal(event.Data.Raw, &subscription)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing webhook JSON: %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		log.Printf("Active entitlement summary updated for %s.", subscription.ID) // Then define and call a func to handle active entitlement summary updated.
+		// handleEntitlementUpdated(subscription)
+	default:
+		fmt.Fprintf(os.Stderr, "Unhandled event type: %s\n", event.Type)
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
 func Init() {
 	fmt.Println("Init")
-	PrintEnv()
+
+	stripe.Key = os.Getenv("SECRET_KEY")
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/create-checkout-session", createCheckoutSession)
+	http.HandleFunc("/create-portal-session", createPortalSession)
+	http.HandleFunc("/webhook", handleWebhook)
+	addr := "localhost:4242"
+	log.Printf("Listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module Tokenize
 
 go 1.23.1
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/stripe/stripe-go v70.15.0+incompatible
+	github.com/stripe/stripe-go/v72 v72.122.0
+	github.com/stripe/stripe-go/v81 v81.0.0
+)
