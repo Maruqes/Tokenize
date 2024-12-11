27d4e318e18e5d27caf50a6c1d7889dfa1bdfe45
Maruqes <m4rques1s0n@gmail.com>
2024-12-11 23:08:37 +0000

    add

---
+++ UserFuncs/userFuncs.go
@@ -0,0 +1,15 @@
+package Users
+
+import "github.com/Maruqes/Tokenize/database"
+
+func GetAllUsers() ([]database.User, error) {
+	return database.GetAllUsers()
+}
+
+func GetUserByID(id int) (database.User, error) {
+	return database.GetUser(id)
+}
+
+func GetUserByEmail(email string) (database.User, error) {
+	return database.GetUserByEmail(email)
+}
