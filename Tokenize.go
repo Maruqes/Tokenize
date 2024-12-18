e07ec918f3b68efad6473ecfa725cab85c548b75
Maruqes <m4rques1s0n@gmail.com>
2024-12-18 10:25:21 +0000

    ad

--- Tokenize.go
+++ Tokenize.go
@@ -462,6 +462,7 @@ func Init(port string, success string, cancel string, typeOfSubscription types.T
 	http.HandleFunc("/health", healthCheck)
 	http.HandleFunc("/getPrecoSub", getPrecoSub)
 
+	
 	addr := "0.0.0.0:" + port
 	log.Printf("Listening on %s", addr)
 
